660602765ca7394f7db3cd21b8492b20c032e933
Jeff See <jeff@revtech.media>
2018-12-21 15:18:16 +1100

    feat(form): Add form component

--- packages/bootstrap/setup/testSetup.js
+++ packages/bootstrap/setup/testSetup.js
@@ -1,8 +1,10 @@
+// // FIXME: this should be from the testSetup file somehow. Not having it breaks things
+// eslint-disable-next-line import/no-extraneous-dependencies
 import 'react-testing-library/cleanup-after-each';
 import 'jest-dom/extend-expect';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'jest-styled-components';
-
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { render } from 'react-testing-library';
 import React from 'react';
 import { obs, fuc, ninesaver, setIn } from '@rtm-ui/theme';
