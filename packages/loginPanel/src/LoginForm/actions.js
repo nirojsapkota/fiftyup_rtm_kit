d024065b992825ad23457e7f631b7a5a96e1e4f2
Hung The <nthehung@tma.com.vn>
2018-12-26 17:45:46 +0700

    OB-2226: buid login form component feat(loginPanel): - Implement render hidden field and call axios to login - Implement display error when failed and redirect when success

--- packages/loginPanel/src/LoginForm/actions.js
+++ packages/loginPanel/src/LoginForm/actions.js
@@ -1,26 +1,28 @@
+// NOTE: there is a rollup bug when compile file with import axios
 // import axios from 'axios';
 const axios = require('axios');
 
-export const submitLogin = (url, data, authenticityToken) => {
+export const submitLogin = async (url, data) => {
   const config = {
     method: 'post',
     url,
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
-      'X-CSRF-Token': authenticityToken,
+      'X-CSRF-Token': data.authenticityToken,
     },
     data,
   };
 
-  return axios(config)
+  const result = await axios(config)
     .then(response => {
       const { data } = response;
-      console.log(response);
       return data;
     })
-    .catch(() => {
-      console.log('error');
-      return null;
+    .catch(error => {
+      const { data } = error.response;
+      return data;
     });
+
+  return result;
 };
--- packages/loginPanel/src/LoginForm/index.js
+++ packages/loginPanel/src/LoginForm/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+// import t from 'prop-types';
 import { Formik, Field } from 'formik';
 import styled from 'styled-components';
 
 import { Box } from '@rtm-ui/layout';
 import Button from '@rtm-ui/button';
+import { Paragraph } from '@rtm-ui/typography';
 
-// import { submitLogin } from './actions';
-// import { LOGIN_URL } from './constants';
+import { submitLogin } from './actions';
+import { LOGIN_URL } from './constants';
 
 const StyledInput = styled.input`
   padding: 8px 2px;
@@ -17,20 +18,50 @@ const StyledInput = styled.input`
 `;
 
 const TextInput = props => <StyledInput {...props} />;
+const HiddenInput = props => <input type="hidden" {...props} />;
+const ErrorWrapper = props => <Paragraph {...props} />;
 
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
 
+    // Set default state
+    this.state = {
+      redirectPath: null,
+      errors: null,
+    };
+
+    // Binding event
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // handleSubmit(values, actions) {
-  // const result = submitLogin(LOGIN_URL, values, this.props.authenticityToken);
-  // actions.isSubmitting(false);
-  // }
+  async handleSubmit(values, actions) {
+    const result = await submitLogin(LOGIN_URL, values);
+    if (result && !result.errors) {
+      if (typeof this.props.handleRedirect === 'function') {
+        this.props.handleRedirect(result);
+      } else {
+        this.setState({
+          redirectPath: result.redirectPath,
+        });
+      }
+    } else {
+      this.setState({
+        errors: result.errors,
+      });
+    }
+    actions.setSubmitting(false);
+  }
 
   render() {
+    const { redirectPath, errors } = this.state;
+    // Redirect to path when success login
+    if (redirectPath) {
+      window.location.href = redirectPath;
+    }
+
+    const { hiddenFields, authenticityToken } = this.props;
+
     return (
       <Formik
         initialValues={{
@@ -38,19 +69,37 @@ class LoginForm extends React.Component {
             postcode_suburb: '',
             email: '',
           },
+          authenticityToken,
+          ...hiddenFields,
         }}
         onSubmit={this.handleSubmit}
-        render={({ handleSubmit, isSubmitting }) => (
+        render={({ values, handleSubmit }) => (
           <form onSubmit={handleSubmit}>
+            {Object.keys(values).map(
+              key =>
+                key !== 'user' && (
+                  <Field
+                    key={key}
+                    name={key}
+                    render={({ field }) => <HiddenInput {...field} id={key} />}
+                  />
+                )
+            )}
             <Box py={2}>
               Join One Big Switch today for FREE
               <br />
               and instantly unlock your special offers!
+              <br />
+              (This text should be configured by entity)
+            </Box>
+            <Box py={2}>
+              {errors &&
+                errors.map(error => <ErrorWrapper>{error}</ErrorWrapper>)}
             </Box>
             <Box py={2}>
               <Box py={2}>My Postcode:</Box>
               <Field
-                name="user[postcode_suburb]"
+                name="user.postcode_suburb"
                 render={({ field }) => (
                   <TextInput
                     {...field}
@@ -65,7 +114,7 @@ class LoginForm extends React.Component {
             <Box py={2}>
               <Box py={2}>My Email:</Box>
               <Field
-                name="user[email]"
+                name="user.email"
                 render={({ field }) => (
                   <TextInput
                     {...field}
@@ -77,9 +126,7 @@ class LoginForm extends React.Component {
                 )}
               />
             </Box>
-            <Button type="submit" disabled={isSubmitting}>
-              See the offer
-            </Button>
+            <Button type="submit">See the offer</Button>
           </form>
         )}
       />
