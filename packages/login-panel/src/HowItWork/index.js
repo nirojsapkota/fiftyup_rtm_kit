efa07e29c787f715e0182f35e63523362b244c4f
Anh Tong <tqanh@tma.com.vn>
2019-01-03 18:07:19 +0700

    Refactor and fix code comment

--- packages/login-panel/src/HowItWork/index.js
+++ packages/login-panel/src/HowItWork/index.js
@@ -8,7 +8,7 @@ const HowItWork = props => (
     <StepOfferStyled>
       {props.stepOffers.map(s => (
         <ItemStyled>
-          <StepImgStyled src={s.imgSrc} />
+          <StepImgStyled src={s.imgUrl} />
           <StepDescStyled>{s.title}</StepDescStyled>
         </ItemStyled>
       ))}
@@ -21,17 +21,17 @@ HowItWork.defaultProps = {
     'One Big Switch takes the stress out of getting value on your household bills by doing the neogtiating for you!',
   stepOffers: [
     {
-      imgSrc:
+      imgUrl:
         'https://www.onebigswitch.com.au/assets/obs-image-assets/pages/home/tick-81a6885ed08fa480d0a3edd9eb3daed0386aeff48e4fd6696bf9d3fd546474e2.png',
       title: 'You join the movement for free',
     },
     {
-      imgSrc:
+      imgUrl:
         'https://www.onebigswitch.com.au/assets/obs-image-assets/pages/home/quote-e31e7443a59255068905462c61ea2fc8c0baf111b4cbd09e7dc0697fa73b605b.png',
       title: 'We negotiate Group Discounts',
     },
     {
-      imgSrc:
+      imgUrl:
         'https://www.onebigswitch.com.au/assets/obs-image-assets/pages/home/dollar-c46a72a93eb371e6000fe5a034cdcd04bddb7c2746bc71ca8220d4e733baecba.png',
       title: 'You decide what’s right for you',
     },
