6fa671e3b7d4a74d51e74c94df539e5fba57ea8b
Hung The <nthehung@tma.com.vn>
2019-01-05 17:22:15 +0700

    Revert "Merge pull request #7 from OBSAU/feature/OB-2226_temp_disable_coverage"
    This reverts commit ccc43a8f5d2406eb85b45a4f69654d3d89615da3, reversing
    changes made to c6cc6f06635852e8698153e5971ae1d5585f1efc.

--- packages/rtm-scripts/scripts/utils/createJestConfig.js
+++ packages/rtm-scripts/scripts/utils/createJestConfig.js
@@ -23,11 +23,10 @@ module.exports = (resolve, rootDir, isEjecting) => {
     coverageReporters: ['json-summary', 'lcov', 'text', 'clover'],
     coverageThreshold: {
       global: {
-        // FIXME: Temporary disable coverage for testing
-        branches: 0,
-        functions: 0,
-        lines: 0,
-        statements: 0,
+        branches: 90,
+        functions: 95,
+        lines: 90,
+        statements: 95,
       },
     },
 
