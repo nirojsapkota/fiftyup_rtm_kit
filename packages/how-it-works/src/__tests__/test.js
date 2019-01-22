fb4b59149551dac256d1b9cac83b3324168b13a9
Anh Tong <tqanh@tma.com.vn>
2019-01-22 15:35:54 +0700

    style(How it works): Added arrow background color for each entity

--- packages/how-it-works/src/__tests__/test.js
+++ packages/how-it-works/src/__tests__/test.js
@@ -4,26 +4,26 @@ import HowItWorks from '../index';
 import howItWorksProps from '../__fixtures__/howItWorks';
 
 describe('<HowItWork />', () => {
+  it('matches expected default content for obs', () => {
+    const content = howItWorksProps.obs;
+    const { getByText, container } = render(<HowItWorks />);
+    expect(getByText(content.header)).toBeInTheDocument();
+    expect(getByText(content.icons[0].title)).toBeInTheDocument();
+    expect(getByText(content.icons[1].title)).toBeInTheDocument();
+    expect(getByText(content.icons[2].title)).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+
   it('matches expected output for entities', () => {
     const entities = ['obs', 'ninesaver', 'fuc'];
     entities.forEach(entity => {
       const content = howItWorksProps[entity];
-      const { getByText, container } = render(<HowItWorks entity={entity} />);
+      const { getByText } = render(<HowItWorks entity={entity} />);
 
       expect(getByText(content.header)).toBeInTheDocument();
       expect(getByText(content.icons[0].title)).toBeInTheDocument();
       expect(getByText(content.icons[1].title)).toBeInTheDocument();
       expect(getByText(content.icons[2].title)).toBeInTheDocument();
-      expect(container).toMatchSnapshot();
     });
-  });
-
-  it('render default content for obs when props is null', () => {
-    const content = howItWorksProps.obs;
-    const { getByText } = render(<HowItWorks />);
-    expect(getByText(content.header)).toBeInTheDocument();
-    expect(getByText(content.icons[0].title)).toBeInTheDocument();
-    expect(getByText(content.icons[1].title)).toBeInTheDocument();
-    expect(getByText(content.icons[2].title)).toBeInTheDocument();
   });
 });
