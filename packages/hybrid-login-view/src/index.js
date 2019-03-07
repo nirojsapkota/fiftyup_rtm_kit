f5c804539b7a31690986ecc72422319ffb661eed
Hung The <nthehung@tma.com.vn>
2019-03-07 22:48:49 +0700

    fix(login-panel): OB-2308 remove unused variable

--- packages/hybrid-login-view/src/index.js
+++ packages/hybrid-login-view/src/index.js
@@ -73,7 +73,6 @@ const HybridLoginView = ({
   entityBrand,
   ...props
 }) => {
-  const loginPanelProps = props;
   return (
     <React.Fragment>
       <BodyWrapper>
@@ -84,7 +83,7 @@ const HybridLoginView = ({
           <MobileHide>
             <Column width={1 / 2}>
               <LoginPanelWrapper px={[10, 10, 15, 20]}>
-                <LoginPanel {...loginPanelProps} />
+                <LoginPanel {...props} />
               </LoginPanelWrapper>
               <StyledDisclaimer p={50}>
                 {disclaimerProps.disclaimerText || ''}
@@ -102,7 +101,7 @@ const HybridLoginView = ({
           </MobileHide>
           <MobileShow>
             <LoginPanelWrapper px={[10, 10, 15, 20]}>
-              <LoginPanel {...loginPanelProps} />
+              <LoginPanel {...props} />
             </LoginPanelWrapper>
             <HowItWorksWrapper px={10} mt={[20, 20, 40, 50]}>
               <HowItWorks {...howItWorksProps} entity={entityBrand} />
