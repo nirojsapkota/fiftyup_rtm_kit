919b4916835f7ca81225591b70f54b02dc162ac6
Priya Devanand <48460603+PriyaNaag@users.noreply.github.com>
2019-04-29 14:37:38 +1000

    fixed readfile export name (#98)
    * fixed readfile export name

--- packages/readfile/src/__tests__/test.js
+++ packages/readfile/src/__tests__/test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { render, wait, fireEvent } from '../../../bootstrap/setup/testSetup';
-import { Readfile } from '../index';
+import { ReadFile } from '../index';
 import axios from 'axios';
-//import readFileUrl from '../api.js';
 
 jest.mock('axios');
 
-describe('<Readfile/>', () => {
+describe('<ReadFile/>', () => {
   it('matches expected list of files from s3', async () => {
     const resp = {
       data: { files: ['k6c3zh83w1/k6c3zh83w1_1554426811442.csv'] },
@@ -29,7 +28,7 @@ describe('<Readfile/>', () => {
       })
     );
     const { getByText, debug } = render(
-      <Readfile readFile={readFile} downloadFile={downloadFile} />
+      <ReadFile readFile={readFile} downloadFile={downloadFile} />
     );
 
     await wait(async () => {
