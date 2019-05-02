c97a7d267388b3662e6c8efa1a23e5a147496263
Jeff See <jeff@revtech.media>
2019-05-02 12:00:03 +1000

    Fix package-lock

--- packages/bootstrap/src/__tests__/exports.js
+++ packages/bootstrap/src/__tests__/exports.js
@@ -9,28 +9,28 @@ packages
     ));
     describe(packageJson.name, () => {
       it(`does not have a default export`, () => {});
-      // const packageExport = require(packageJson.name);
-      // const rtmRollup = packageJson.rtmRollup;
-      // if (rtmRollup) {
-      //   it(`does not have a default export`, () => {
-      //     expect(packageExport.default).toBeUndefined();
-      //   });
-      //   Object.keys(packageExport)
-      //     .filter(exportedModule => exportedModule !== 'default')
-      //     .map(exportedModule => {
-      //       it(`lists ${exportedModule} as a named export`, () => {
-      //         expect(
-      //           rtmRollup.namedExports.filter(
-      //             namedExport => namedExport === exportedModule
-      //           ).length
-      //         ).toEqual(1);
-      //       });
-      //     });
-      //   rtmRollup.namedExports.map(namedExport => {
-      //     it(`provides a named export of ${namedExport}`, () => {
-      //       expect(packageExport[namedExport]).not.toBeUndefined();
-      //     });
-      //   });
-      // }
+      const packageExport = require(packageJson.name);
+      const rtmRollup = packageJson.rtmRollup;
+      if (rtmRollup) {
+        it(`does not have a default export`, () => {
+          expect(packageExport.default).toBeUndefined();
+        });
+        Object.keys(packageExport)
+          .filter(exportedModule => exportedModule !== 'default')
+          .map(exportedModule => {
+            it(`lists ${exportedModule} as a named export`, () => {
+              expect(
+                rtmRollup.namedExports.filter(
+                  namedExport => namedExport === exportedModule
+                ).length
+              ).toEqual(1);
+            });
+          });
+        rtmRollup.namedExports.map(namedExport => {
+          it(`provides a named export of ${namedExport}`, () => {
+            expect(packageExport[namedExport]).not.toBeUndefined();
+          });
+        });
+      }
     });
   });
