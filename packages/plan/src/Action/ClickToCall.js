27291d5ccb98016b50e1473964abc22a908dd04e
Huyen Pham <ptthuyen@tma.com.vn>
2019-06-21 11:14:10 +0700

    - Move Variant to Plan

--- packages/plan/src/Action/ClickToCall.js
+++ packages/plan/src/Action/ClickToCall.js
@@ -3,37 +3,34 @@ import PropTypes from 'prop-types';
 import { A } from '@rtm-ui/a';
 import { Header, Paragraph } from '@rtm-ui/typography';
 import { Icon } from '@rtm-ui/icon';
-import { Theme as Variant } from '@rtm-ui/theme';
 
 const ClickToCall = ({ header, link, footer, track }) => {
   return (
-    <Variant variant="regular"> 
-      <React.Fragment>
-        {header && (
-          <Paragraph
-            color="secondary"
-            align="center"
-            px={[2, 3]}
-            pt={2}
-            dangerousHTML={header}
-          />
-        )}
-        <A track={track} href={`tel:${link}`}>
-          <Header py={[1]} align="center" tag="h3">
-            <Icon glyph="phone" fill="primary" inline /> {link}
-          </Header>
-        </A>
-        {footer && (
-          <Paragraph
-            dangerousHTML={footer}
-            color="secondary"
-            align="center"
-            pb={[2, 2]}
-            px={[2, 3]}
-          />
-        )}
-      </React.Fragment>
-    </Variant>
+    <React.Fragment>
+      {header && (
+        <Paragraph
+          color="secondary"
+          align="center"
+          px={[2, 3]}
+          pt={2}
+          dangerousHTML={header}
+        />
+      )}
+      <A track={track} href={`tel:${link}`}>
+        <Header py={[1]} align="center" tag="h3">
+          <Icon glyph="phone" fill="primary" inline /> {link}
+        </Header>
+      </A>
+      {footer && (
+        <Paragraph
+          dangerousHTML={footer}
+          color="secondary"
+          align="center"
+          pb={[2, 2]}
+          px={[2, 3]}
+        />
+      )}
+    </React.Fragment>
   );
 };
 
