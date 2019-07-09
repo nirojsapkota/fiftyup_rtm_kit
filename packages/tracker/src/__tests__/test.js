85c072acb05361a08eb0a0363647c393b148be80
s11shweta <43434472+s11shweta@users.noreply.github.com>
2019-07-10 09:49:57 +1000

    Shweta/ob 2653 1 (#177)
    * deleted tracker-registration package
    
    * added tracker registration inside tracker package

--- packages/tracker/src/__tests__/test.js
+++ packages/tracker/src/__tests__/test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import LogRocket from 'logrocket';
 // eslint-disable-next-line import/named
 import { render, cleanup, fireEvent } from '../../../bootstrap/setup/testSetup';
-import { Tracker, TrackingProvider, useTracker, track } from '..';
+import { Tracker, TrackingProvider, useTracker, track, TrackerRegistration } from '..';
 import Google from '../google';
 import Facebook from '../facebook';
 
@@ -129,5 +129,26 @@ describe(`<Tracker />`, () => {
     fireEvent.click(getByText('Hello'));
 
     expect(buttonEvent).toHaveBeenCalled();
+  });
+});
+
+describe('<TrackerRegistration />', () => {
+
+  it(`renders the script with given IDs`, () => {
+
+    const ga_code = 'UA-121324450-2';
+    const bing_uet_tag_code = '25041030';
+    const google_adwords_id = 'AW-964414963';
+    const facebook_pixel_id = '1111111111';
+    const zendesk_id = 'a85c71b0-2af3-4bb0-9cd3-3a9eb0ebcb67';
+
+    const { getByTestId } = render(<TrackerRegistration ga_code={ga_code} bing_uet_tag_code={bing_uet_tag_code} google_adwords_id={google_adwords_id} facebook_pixel_id={facebook_pixel_id} zendesk_id={zendesk_id} />);
+
+    expect(getByTestId('TrackingRegister').innerHTML).toContain(ga_code);
+    expect(getByTestId('TrackingRegister').innerHTML).toContain(bing_uet_tag_code);
+    expect(getByTestId('TrackingRegister').innerHTML).toContain(google_adwords_id);
+    expect(getByTestId('TrackingRegister').innerHTML).toContain(facebook_pixel_id);
+    expect(getByTestId('TrackingRegister').innerHTML).toContain(zendesk_id);
+
   });
 });
--- packages/tracker/src/index.js
+++ packages/tracker/src/index.js
@@ -72,6 +72,83 @@ export const Tracker = props => {
   );
 };
 
+export const TrackerRegistration = props => {
+  
+  return (<div data-testid="TrackingRegister" {...props} >
+    
+    <script>
+      {
+        `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', '${props.ga_code}', 'auto');
+        ga('send', 'pageview');`
+    }
+    </script>
+
+    <script>
+      {
+        `(function (w, d, t, r, u) {
+          var f, n, i;
+          w[u] = w[u] || [], f = function () {
+            var o = {ti: '${props.bing_uet_tag_code}'};
+            o.q = w[u], w[u] = new UET(o), w[u].push("pageLoad")
+          }, n = d.createElement(t), n.src = r, n.async = 1, n.onload = n.onreadystatechange = function () {
+            var s = this.readyState;
+            s && s !== "loaded" && s !== "complete" || (f(), n.onload = n.onreadystatechange = null)
+          }, i = d.getElementsByTagName(t)[0], i.parentNode.insertBefore(n, i)
+        })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
+        window.uetq = window.uetq || [];
+        window.uetq.push ('event', 'pageview');`
+      }
+    </script>
+    
+   
+    <script async src={`https://www.googletagmanager.com/gtag/js?id='${props.google_adwords_id}'`}></script>
+    <script>
+      {
+        `window.dataLayer = window.dataLayer || [];
+        function gtag() {" dataLayer.push(arguments);"}
+        gtag('js', new Date());
+        gtag('config', '${props.google_adwords_id}');`
+      }
+    </script>
+
+    <script type="text/javascript">
+      {
+        `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+        n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+        document,'script','https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '${props.facebook_pixel_id}');
+        fbq('track', 'PageView');`
+      }
+    </script>
+    <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${props.facebook_pixel_id}&amp;ev=PageView&amp;noscript=1" />` }} />
+
+    <script type="text/javascript">
+      {
+        `window.zESettings = {
+          webWidget: {
+            contactOptions: {
+              enabled: true,
+            contactButton: { '*': 'Contact Button' },
+            chatLabelOnline: { '*': 'Live Chat' },
+            chatLabelOffline: { '*': 'Chat is unavailable' },
+            contactFormLabel: { '*': 'Leave us a message' }
+            }
+          }
+        };`
+      }
+    </script>
+    <script id="ze-snippet" src={`https://static.zdassets.com/ekr/snippet.js?key='${props.zendesk_id}'`}></script>
+
+  </div>)
+  
+};
+
 Tracker.propTypes = {
   render: PropTypes.func,
 };
@@ -79,4 +156,12 @@ Tracker.propTypes = {
 TrackingProvider.propTypes = {
   children: PropTypes.node,
   trackingData: PropTypes.shape({ category: PropTypes.string.isRequired }),
+};
+
+TrackerRegistration.propTypes = {
+  ga_code: PropTypes.string,
+  bing_uet_tag_code: PropTypes.string,
+  google_adwords_id: PropTypes.string,
+  facebook_pixel_id: PropTypes.string,
+  zendesk_id: PropTypes.string
 };
