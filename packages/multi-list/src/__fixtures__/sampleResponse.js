98a5d3649e0a7820106545736b9effb7000e1333
Priya <priya.devanand@revtech.media>
2019-07-15 17:09:26 +1000

    Added new Multi list component

---
+++ packages/multi-list/src/__fixtures__/sampleResponse.js
@@ -0,0 +1,30 @@
+const data = {
+  header: ' What being a One Big Switch member means for you',
+  subHeader: 'See why so many Australians are joining One Big Switch',
+  items: [
+    {
+      icon: 'thumbs-up',
+
+      title: '##### Joining is easy',
+
+      body:
+        'Joining One Big Switch is as easy as typing out your email address and postcode. Theres no cost and no obligation. Youre simply throwing your hat in the ring.',
+    },
+    {
+      icon: 'inbox',
+
+      title: '##### Deals direct to your inbox',
+      body:
+        ' As a member of One Big Switch, you get exclusive access to big discounts and special offers unlocked by the huge buying power of over 1 million One Big Switch members.',
+    },
+    {
+      icon: 'savings',
+
+      title: '##### Cash in your pocket',
+      body:
+        'One Big Switchs main game is saving our members big on their household bills. Since we launched in 2011, we have saved our members thousands of dollars. Dont miss out!',
+    },
+  ],
+};
+
+export default data;
