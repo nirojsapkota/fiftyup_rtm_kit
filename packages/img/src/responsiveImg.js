6dc376d7e453fdfc1505487bfe9f6569409d7248
Priya <priya.devanand@revtech.media>
2019-08-01 16:24:05 +1000

    Updated image component

--- packages/img/src/responsiveImg.js
+++ packages/img/src/responsiveImg.js
@@ -1,3 +1,7 @@
+/*
+Responsive Image component to be used inside the Home Page
+*/
+
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '@rtm-ui/layout';
