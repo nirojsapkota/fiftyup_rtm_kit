d9e3710b84cb24b75ebd4736c6656da217396c14
Jeff See <jeff@revtech.media>
2019-08-08 15:48:31 +1000

    fix hero test

--- packages/hero/src/__tests__/test.js
+++ packages/hero/src/__tests__/test.js
@@ -4,17 +4,15 @@ import { Hero } from '../index';
 import { heroProps } from '../__fixtures__/heroProps';
 
 describe('<Hero />', () => {
-
   it('matches expected output', () => {
-    
-    const { queryByText, container } = render(<Hero {...heroProps} />);
+    const { queryByText, getByTestId } = render(<Hero {...heroProps} />);
 
     expect(queryByText(heroProps.imgText)).toBeInTheDocument();
     expect(queryByText(heroProps.buttonText)).toBeInTheDocument();
 
-    const background = container.childNodes[1].style['background-image'];
+    const backgroundNode = getByTestId('hero-background');
+    const background = backgroundNode.style['background-image'];
     expect(background).not.toBe(undefined);
     expect(background).toEqual(`url(${heroProps.backgroundImgUrl})`);
   });
-
-})
+});
--- packages/hero/src/index.js
+++ packages/hero/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Header } from '@rtm-ui/typography'
-import { Box } from '@rtm-ui/layout'
+import { Header } from '@rtm-ui/typography';
+import { Box } from '@rtm-ui/layout';
 import { Button } from '@rtm-ui/button';
 
 const HeroWrapper = styled(Box)`
@@ -12,24 +12,33 @@ const HeroWrapper = styled(Box)`
   height: 454px;
 `;
 
-export const Hero = ({...props}) => {
-  
+export const Hero = ({ ...props }) => {
   return (
     <React.Fragment>
-    <HeroWrapper style={{"backgroundImage": `url(${props.backgroundImgUrl})`, "backgroundPosition": "center"}}>
-      <Header align="center" mt={130}>{props.imgText}</Header>
-      <Box mx="auto" mt={80}>
-        <Button as="a" href={props.buttonLink}>{props.buttonText}</Button>
-      </Box>
-    </HeroWrapper>    
+      <HeroWrapper
+        data-testid="hero-background"
+        style={{
+          backgroundImage: `url(${props.backgroundImgUrl})`,
+          backgroundPosition: 'center',
+        }}
+      >
+        <Header align="center" mt={130}>
+          {props.imgText}
+        </Header>
+        <Box mx="auto" mt={80}>
+          <Button as="a" href={props.buttonLink}>
+            {props.buttonText}
+          </Button>
+        </Box>
+      </HeroWrapper>
     </React.Fragment>
   );
-}
+};
 
 Hero.propTypes = {
   children: PropTypes.node,
   backgroundImgUrl: PropTypes.string,
   imgText: PropTypes.string,
   buttonText: PropTypes.string,
-  buttonLink: PropTypes.string
+  buttonLink: PropTypes.string,
 };
