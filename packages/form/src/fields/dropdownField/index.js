f48b5094eef84de266e47211ba4caabf19fdbfab
Dhaivat Parikh <dhaivat28@gmail.com>
2019-11-11 15:11:32 +1100

    Mapped component

---
+++ packages/form/src/fields/dropdownField/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DropdownField = () => {
+  return (<h1>DropdownField</h1>);
+};
+
+export default DropdownField;
--- packages/form/src/fields/util/getFieldComponent.js
+++ packages/form/src/fields/util/getFieldComponent.js
@@ -1,13 +1,14 @@
-import RadioField from '../radioField';
-import TextField from '../textField';
-import NumberField from '../numberField';
-import StripeField from '../stripeField';
-import CheckboxField from '../checkboxField';
 import AutocompletField from '../autocompleteField';
+import CheckboxField from '../checkboxField';
+import DropdownField from '../dropdownField';
 import HiddenField from '../hiddenField';
+import MonthField from '../monthField';
+import NumberField from '../numberField';
 import PanelCheckField from '../panelCheckField';
 import PanelRadioField from '../panelRadioField';
-import MonthField from '../monthField';
+import RadioField from '../radioField';
+import StripeField from '../stripeField';
+import TextField from '../textField';
 
 export const getFieldComponent = (
   type,
@@ -18,18 +19,20 @@ export const getFieldComponent = (
       ? PanelRadioField
       : RadioField
     : type === 'checkbox'
-    ? component === 'panelCheck'
-      ? PanelCheckField
-      : CheckboxField
-    : component === 'month'
-    ? MonthField
-    : type === 'hidden'
-    ? HiddenField
-    : component === 'stripePayment'
-    ? StripeField
-    : component === 'autocomplete'
-    ? AutocompletField
-    : validator === 'mask'
-    ? NumberField
-    : TextField;
+      ? component === 'panelCheck'
+        ? PanelCheckField
+        : CheckboxField
+      : component === 'month'
+        ? MonthField
+        : type === 'hidden'
+          ? HiddenField
+          : component === 'stripePayment'
+            ? StripeField
+            : component === 'autocomplete'
+              ? AutocompletField
+              : component === 'dropdownfield'
+                ? DropdownField
+                : validator === 'mask'
+                  ? NumberField
+                  : TextField;
 };
