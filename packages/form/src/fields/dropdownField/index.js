9d8767d9658560c97a1d2d01c133a4a30a00f466
Dhaivat Parikh <dhaivat28@gmail.com>
2019-11-11 16:07:13 +1100

    working dropdown

--- packages/form/src/fields/dropdownField/index.js
+++ packages/form/src/fields/dropdownField/index.js
@@ -4,8 +4,6 @@ import { Header, Label } from '@rtm-ui/typography';
 import React from 'react';
 import styled from 'styled-components';
 import TextField from '../textField';
-import { useDebounce } from './useDebounce';
-import { useOnClickOutside } from './useOnClickOutside';
 
 const ResultsContainer = styled(Card)`
   position: absolute;
@@ -24,97 +22,81 @@ const ResultItem = styled(Button)`
   }
 `;
 
-const DropdownField = ({
-  onWaiting,
+const DropdownField = ({ onWaiting,
   onFocus,
   onBlur,
   fieldUtils,
   config,
-  ...inputProps
-}) => {
+  ...inputProps }) => {
 
-  console.log(props);
+  console.log(inputProps.options);
 
-  const resultsRef = React.useRef();
+
   const inputRef = React.useRef();
-  const [results, setResults] = React.useState([]);
   const [hasSelected, setHasSelected] = React.useState(false);
   const [isModalOpen, setModalOpen] = React.useState(false);
   const [resultsPosition, setResultsPosition] = React.useState();
-  useOnClickOutside(resultsRef, () => setModalOpen(false));
-  const debouncedSearchTerm = useDebounce(inputProps.value, 500);
   const [hasInteracted, setHasInteracted] = React.useState(false);
 
-  React.useEffect(() => {
-    if (debouncedSearchTerm) {
-      !hasSelected && onWaiting('Searching pending...');
-      config.searchFunction(debouncedSearchTerm).then(results => {
-        results.length > 0
-          ? isModalOpen
-            ? onWaiting(`${results.length} results`)
-            : onWaiting(``)
-          : (onWaiting(``), config.onEmptyResult());
-        setResults(results);
-      });
-    } else {
-      onWaiting('');
-      setResults([]);
-    }
-  }, [debouncedSearchTerm]);
-
-  React.useEffect(() => {
-    if (inputProps.value && !hasSelected && hasInteracted) {
-      // onWaiting('Search pending');
-      setModalOpen(true);
-    } else {
-      setModalOpen(false);
-    }
-  }, [inputProps.value]);
-
-  React.useEffect(() => {
-    if (inputRef.current) {
-      const position = inputRef.current.getBoundingClientRect();
-      if (position) {
-        setResultsPosition(position.height + 20);
-      }
-    }
-  }, [inputRef]);
+  console.log("isModalOpen", isModalOpen);
+  console.log("setHasSelected", hasSelected);
 
   return (
     <div style={{ position: 'relative' }}>
+
       <div ref={inputRef}>
         <TextField
           {...inputProps}
+
           aria-haspopup="listbox"
+
           onFocus={() => {
             setModalOpen(true);
             setHasInteracted(true);
             onFocus();
+
           }}
-          onBlur={onBlur}
+
+          onBlur={() => {
+            setModalOpen(false);
+            onBlur();
+          }}
+
           onChange={e => {
             setHasSelected(false);
             setHasInteracted(true);
             inputProps.onChange(e);
           }}
+
         />
       </div>
+
+
       {isModalOpen && (
-        <div ref={resultsRef}>
+        <div>
           <ResultsContainer distanceFromTop={resultsPosition}>
-            {results.map((result, index) => {
+
+
+            {inputProps.options.map((result, index) => {
               return (
                 <Label
                   htmlFor={`result-${inputProps.name}-${index}`}
                   key={result.label}
                 >
                   <ResultItem
+
                     aria-selected={result.label === inputProps.value}
+
                     asWrapper
+
                     id={`result-${inputProps.name}-${index}`}
+
                     type="button"
+
                     block
+
                     style={{ width: '100%' }}
+
                     onClick={() => {
                       setHasSelected(true);
                       setModalOpen(false);
@@ -127,6 +109,7 @@ const DropdownField = ({
                     <Header tag="h6" align="left" weight="thin" p={15}>
                       {result.label}
                     </Header>
+
                   </ResultItem>
                 </Label>
               );
