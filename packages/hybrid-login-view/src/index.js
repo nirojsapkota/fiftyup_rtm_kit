969d4efb1290a9482f877f7d18687474c8171ce7
s11shweta <shweta@revtech.media>
2021-04-14 11:22:31 +1000

    fix(hybrid-login-view): The top spacing increased for the login box so that it dos not overlap the b

--- packages/hybrid-login-view/src/index.js
+++ packages/hybrid-login-view/src/index.js
@@ -65,7 +65,7 @@ const LoginPanelWrapper = styled(Box)`
   @media (min-width: ${props => props.theme.grid.md}em) {
     left: 58%; /* Fallback if needed */
     left: calc(50% + 90px);
-    top: -125px;
+    top: -85px;
     position: absolute;
   }
   @media (min-width: ${props => props.theme.grid.lg}em) {
@@ -205,7 +205,7 @@ const HeadingSection = ({ mainHeading, asSeenOnImage }) => {
               <ImageWrapper m="auto">
                 <Img src={asSeenOnImage} alt="As Seen On" onClick={e => {
                   scrollToElement(e, 'login-panel');
-                }}/>
+                }} />
               </ImageWrapper>
             )}
           </Box>
@@ -275,41 +275,41 @@ const HybridLoginView = ({
             {(workflowOffer.header ||
               workflowOffer.items.length > 0 ||
               accordion.length > 0) && (
-              <ContainerWrapper className="content-wrapper">
-                <ContentWrapper>
-                  <Box {...defaultProps}>
-                    <Variant variant="a">
-                      <WorkFlowContainer>
-                        <WorkFlow 
-                          multiContent
-                          scrollTo="login-panel"
-                          header={workflowOffer.header}
-                          items={workflowOffer.items}
-                        />
-                      </WorkFlowContainer>
-                    </Variant>
+                <ContainerWrapper className="content-wrapper">
+                  <ContentWrapper>
+                    <Box {...defaultProps}>
+                      <Variant variant="a">
+                        <WorkFlowContainer>
+                          <WorkFlow
+                            multiContent
+                            scrollTo="login-panel"
+                            header={workflowOffer.header}
+                            items={workflowOffer.items}
+                          />
+                        </WorkFlowContainer>
+                      </Variant>
 
-                    {accordion.length > 0 && (
-                      <Column variant="b" pb="20px">
-                        <Accordion
-                          items={accordion}
-                          renderItem={item => (
-                            <Variant variant="a">
-                              <Box p={[2, 2, 3]}>
-                                <MarkdownWrapper content={item.content} />
-                              </Box>
-                            </Variant>
-                          )}
-                          renderHeader={item => (
-                            <Header tag="h5">{item.name}</Header>
-                          )}
-                        />
-                      </Column>
-                    )}
-                  </Box>
-                </ContentWrapper>
-              </ContainerWrapper>
-            )}
+                      {accordion.length > 0 && (
+                        <Column variant="b" pb="20px">
+                          <Accordion
+                            items={accordion}
+                            renderItem={item => (
+                              <Variant variant="a">
+                                <Box p={[2, 2, 3]}>
+                                  <MarkdownWrapper content={item.content} />
+                                </Box>
+                              </Variant>
+                            )}
+                            renderHeader={item => (
+                              <Header tag="h5">{item.name}</Header>
+                            )}
+                          />
+                        </Column>
+                      )}
+                    </Box>
+                  </ContentWrapper>
+                </ContainerWrapper>
+              )}
           </div>
 
           {(workflow.header || workflow.items.length > 0) && (
