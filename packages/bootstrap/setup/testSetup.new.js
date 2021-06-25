f3215aa97524eac484735727d30a163786d7d836
Prajwal joshi <prazwal@outlook.com>
2021-06-25 15:48:18 +1000

    test(LoginCalculator panel): skip test for testing

--- packages/bootstrap/setup/testSetup.new.js
+++ packages/bootstrap/setup/testSetup.new.js
@@ -1,36 +1,36 @@
-// /**
-//  * NEW TEST SETUP
-//  *
-//  * ! WHY?
-//  *
-//  * Because "react-testing-library" was deprecaited in 2019
-//  *
-//  * "@testing-library/react" is the successor to the "react-testing-library".
-//  *
-//  *
-//  */
+/**
+ * NEW TEST SETUP
+ *
+ * ! WHY?
+ *
+ * Because "react-testing-library" was deprecaited in 2019
+ *
+ * "@testing-library/react" is the successor to the "react-testing-library".
+ *
+ *
+ */
 
-// // eslint-disable-next-line import/no-extraneous-dependencies
-// import 'jest-styled-components';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'jest-styled-components';
 
-// import '@testing-library/jest-dom'; // (Replaces) 'jest-dom/extend-expect';
+import '@testing-library/jest-dom'; // (Replaces) 'jest-dom/extend-expect';
 
-// import { render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
-// import React from 'react';
-// import TestBootstrap from './docSetup';
+import React from 'react';
+import TestBootstrap from './docSetup';
 
-// const bootstrapRender = (
-//   node,
-//   { theme = 'obs', themeOverrides, ...options } = {}
-// ) => {
-//   return render(
-//     <TestBootstrap themeName={theme} overrides={themeOverrides}>
-//       {node}
-//     </TestBootstrap>,
-//     options
-//   );
-// };
+const bootstrapRender = (
+  node,
+  { theme = 'obs', themeOverrides, ...options } = {}
+) => {
+  return render(
+    <TestBootstrap themeName={theme} overrides={themeOverrides}>
+      {node}
+    </TestBootstrap>,
+    options
+  );
+};
 
-// export * from '@testing-library/react';
-// export { bootstrapRender as render };
+export * from '@testing-library/react';
+export { bootstrapRender as render };
