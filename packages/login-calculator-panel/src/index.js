5a9e9b56616e1b572c7e3b3666ec8ee054c6d18d
L Jacobs <41974495+Jeecabs@users.noreply.github.com>
2021-07-01 10:33:42 +1000

    Typo fix

--- packages/login-calculator-panel/src/index.js
+++ packages/login-calculator-panel/src/index.js
@@ -39,7 +39,7 @@ const ButtonWrapper = styled(Box)`
 const CalculatorPanelWrapper = styled(Box)`
   height: 100%;
   overflow: unset;
-  padding: 0px 50px 00px 0px;
+  padding: 0px 50px 0px 0px;
 
   @media screen and (max-width: 750px) {
     padding: 0px 0px 0px 0px;
