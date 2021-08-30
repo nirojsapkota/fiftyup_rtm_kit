7a2d29d12d4e95adea8825ad3bc233cf3bdb1187
Praj <work.prajwal.joshi@gmail.com>
2021-08-30 12:24:20 +1000

    test(Carousel): basic test

--- packages/carousel/src/__tests__/test.js
+++ packages/carousel/src/__tests__/test.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import { render } from '../../../bootstrap/setup/testSetup';
+// eslint-disable-next-line import/named
+import { render, shallow } from '../../../bootstrap/setup/testSetup';
 import Carousel from '../index';
 
 describe('<Carousel />', () => {
-  it('has unit tests specified', () => {
-    expect(true).toEqual(false);
+  const props = {
+    duration: 4,
+    slides: [
+      {
+        image:
+          'https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
+        altText: 'image1',
+        controlColor: '#fff',
+      },
+      {
+        image:
+          'https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        altText: 'image2',
+        controlColor: '#fff',
+      },
+      {
+        image:
+          'https://images.unsplash.com/photo-1475189778702-5ec9941484ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80',
+        altText: 'image3',
+        controlColor: '#fff',
+      },
+    ],
+  };
+  it('renders expected images', () => {
+    const carousel = shallow(<Carousel {...props} />);
+    expect(carousel.find('img').prop('src')).toEqual(slides[0].image);
+    expect(carousel.find('img').prop('alt')).toEqual(slides[0].alt);
   });
 });
