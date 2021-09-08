f934fb4110dad64d9b58b9ca54d88e45249b6f42
Praj <work.prajwal.joshi@gmail.com>
2021-09-08 12:00:44 +1000

    add min-height to image wrapper in carousel

--- packages/carousel/src/index.js
+++ packages/carousel/src/index.js
@@ -15,6 +15,7 @@ const SliderBox = styled(Box)`
 const ImageWrapper = styled('div')`
   opacity: 0.5;
   transition-duration: 1s ease;
+  min-height: 350px;
   ${props =>
     props.isActive &&
     css`
@@ -27,7 +28,7 @@ const ImageWrapper = styled('div')`
 
 const Image = styled('img')`
   width: 100%;
-  min-height: 350px;
+  height: auto
   margin: auto;
 `;
 
