96bb2ea2b6690d97bb22c891278b98e04770e9d8
Gerda Decio <gerdadecio@gmail.com>
2021-09-24 15:10:08 +1000

    test(LoginPanel): Update tests

--- packages/login-panel/src/__tests__/test.js
+++ packages/login-panel/src/__tests__/test.js
@@ -282,7 +282,7 @@ describe('<LoginPanel />', () => {
       placeholder: 'Email',
     };
 
-    const { getByText, getByValue } = render(
+    const { getByText, getByDisplayValue } = render(
       <LoginPanel
         {...loginPanelExitIntentProps}
         stateField={postCodeField}
@@ -293,9 +293,9 @@ describe('<LoginPanel />', () => {
     const { hiddenFields } = loginPanelProps;
 
     // expect hidden fields
-    const jumpPath = getByValue(hiddenFields.jump_path);
+    const jumpPath = getByDisplayValue(hiddenFields.jump_path);
     expect(jumpPath.name).toEqual('jump_path');
-    const registeringCampaignId = getByValue(
+    const registeringCampaignId = getByDisplayValue(
       hiddenFields.registering_campaign_id.toString()
     );
     expect(registeringCampaignId.name).toEqual('registering_campaign_id');
