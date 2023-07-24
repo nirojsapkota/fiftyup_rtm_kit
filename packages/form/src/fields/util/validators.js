85692d46a6bc1af0cb410be109e65bd4c41116b3
iamgeeeee <gerdadecio@gmail.com>
2023-07-24 17:16:14 +1000

    fix(Form): Update the lifeInsuranceAgeDropdown validation
    Not validating properly for custom age range

--- packages/form/src/fields/util/validators.js
+++ packages/form/src/fields/util/validators.js
@@ -112,9 +112,11 @@ const rangeArr = (start, end) => {
     });
 };
 
-export const lifeInsuranceAgeDropdownValidator = Yup.string()
-  .oneOf(
-    rangeArr(16, 69),
-    'Sorry, but you must be between 16 and 69 years old to be eligible for this offer.'
-  )
-  .required('Required');
+export const lifeInsuranceAgeDropdownValidator = (minAge = 16, maxAge = 69) => {
+  return Yup.string()
+    .oneOf(
+      rangeArr(minAge, maxAge),
+      `Sorry, but you must be between ${minAge} and ${maxAge} years old to be eligible for this offer.`
+    )
+    .required('Required');
+};
