d5092364ae2258b6d2fec6ae751ae8d44c91f149
iamgeeeee <gerdadecio@gmail.com>
2023-08-07 17:11:22 +1000

    improvement(Form): Add a yearField component

---
+++ packages/form/src/fields/yearField/index.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import TextField from '../textField';
+import styled from 'styled-components';
+
+const StyledField = styled(TextField)`
+  width: 20px;
+  display: inline;
+  margin: 5px;
+`;
+
+const YearField = ({ fieldUtils, defaultValue, ...props }) => {
+  const [year, setYear] = useState([]);
+
+  useEffect(() => {
+    fieldUtils.setFieldValue(props.name, defaultValue);
+  }, []);
+
+  const handleChange = (index, { target: { value } }) => {
+    if (value.length < 2 && (!isNaN(Number(value)) || value == '')) {
+      let updatedYear = year;
+      updatedYear[index] = value;
+      setYear(updatedYear);
+      fieldUtils.setFieldValue(
+        props.name,
+        `${year[0]}${year[1]}${year[2]}${year[3]}`
+      );
+
+      const element = event.target;
+      const nextSibling = element.nextElementSibling;
+      nextSibling ? nextSibling.focus() : element.blur();
+    } else {
+      setYear(year);
+      fieldUtils.setFieldValue(
+        props.name,
+        `${year[0]}${year[1]}${year[2]}${year[3]}`
+      );
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <div>
+        <StyledField
+          value={year[0]}
+          placeholder="Y"
+          onChange={e => handleChange(0, e)}
+        />
+        <StyledField
+          value={year[1]}
+          placeholder="Y"
+          onChange={e => handleChange(1, e)}
+        />
+        <StyledField
+          value={year[2]}
+          placeholder="Y"
+          onChange={e => handleChange(2, e)}
+        />
+        <StyledField
+          value={year[3]}
+          placeholder="Y"
+          onChange={e => handleChange(3, e)}
+        />
+        <TextField type="hidden" name={props.name} value="" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+YearField.PropTypes = {
+  id: PropTypes.string,
+  defaultValue: PropTypes.string,
+};
+
+export default YearField;
